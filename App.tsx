9b40a8a1ef66952e65b00faad9576a78bf74c8f3
SAHA <pkomom123@naver.com>
2024-05-12 20:14:26 +0900

    ui: style apply method

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState('');
+  const [lists, setLists] = useState<string[]>([]);
+
+  const listInputHandler = (inputText: string) => {
+    setEnteredText(inputText);
+  };
+
+  const addListHandler = () => {
+    setLists((currentList) => [enteredText, ...currentList]);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='너의 목표는 뭐니?!' />
-        <Button title='이뤄봐!' />
+        <TextInput
+          style={styles.textInput}
+          placeholder='너의 목표는 뭐니?!'
+          onChangeText={listInputHandler}
+        />
+        <Button title='이뤄봐!' onPress={addListHandler} />
       </View>
       <View style={styles.listContainer}>
-        <Text>너의 목표들이야!</Text>
+        {lists.map((list, i) => (
+          <View key={i} style={styles.listItem}>
+            <Text>{list}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -39,5 +59,14 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 7,
+  },
+  listItem: {
+    padding: 16,
+    backgroundColor: '#f9f9f9',
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 8,
+    color: 'black',
   },
 });
