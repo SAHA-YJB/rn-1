4c58aa16e3901b1085427128bbf21ec6f6eeb5e4
SAHA <pkomom123@naver.com>
2024-05-14 16:01:37 +0900

    ListInput component

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
 import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
 import ListItem from './src/components/ListItem';
+import ListInput from './src/components/ListInput';
 
 type ListType = { text: string; key?: string; id?: string };
 
 export default function App() {
-  const [enteredText, setEnteredText] = useState('');
   const [lists, setLists] = useState<ListType[]>([]);
 
-  const listInputHandler = (inputText: string) => {
-    setEnteredText(inputText);
-  };
-
-  const addListHandler = () => {
+  const addListHandler = (enteredText: string) => {
     setLists((currentList) => [
       { text: enteredText, id: Math.random().toString() },
       ...currentList,
@@ -21,14 +17,7 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='너의 목표는 뭐니?!'
-          onChangeText={listInputHandler}
-        />
-        <Button title='이뤄봐!' onPress={addListHandler} />
-      </View>
+      <ListInput addListHandler={addListHandler} />
       <View style={styles.listContainer}>
         <FlatList
           data={lists}
@@ -51,33 +40,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    width: '80%',
-    borderColor: '#cccccc',
-    borderWidth: 1,
-    borderRadius: 4,
-    padding: 8,
-    marginRight: 8,
-  },
   listContainer: {
     flex: 7,
-  },
-  listItem: {
-    padding: 16,
-    backgroundColor: '#f9f9f9',
-    borderColor: '#cccccc',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 8,
-    color: 'black',
   },
 });
--- src/components/ListInput.tsx
+++ src/components/ListInput.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
-const ListInput = () => {
-  return;
+interface ListInputProps {
+  addListHandler: (enteredText: string) => void;
+}
+
+const ListInput: FC<ListInputProps> = ({ addListHandler }) => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const listInputHandler = (inputText: string) => {
+    setEnteredText(inputText);
+  };
+
+  const addListHandlerInput = () => {
+    addListHandler(enteredText);
+    setEnteredText('');
+  };
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder='너의 목표는 뭐니?!'
+        onChangeText={listInputHandler}
+        value={enteredText}
+      />
+      <Button title='이뤄봐!' onPress={addListHandlerInput} />
+    </View>
+  );
 };
 
 export default ListInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    width: '80%',
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 8,
+    marginRight: 8,
+  },
+});
